9691c4cb7075afdc215c331965a78cb8e051bf44
mokhlesurr031 <mokhlesurr031@gmail.com>
2022-05-29 00:27:54 +0600

    movie details data getting fetched

--- scrap.py
+++ scrap.py
@@ -2,7 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 from urllib.parse import urljoin
 
-def fetch_initial_data():
+
+def get_movie_list_data():
     movie_list_url = 'https://en.wikipedia.org/wiki/List_of_Academy_Award-winning_films'
     response = requests.get(movie_list_url)
     soup = BeautifulSoup(response.text, "lxml")
@@ -15,6 +16,8 @@ def fetch_initial_data():
             link = urljoin('https://en.wikipedia.org', link)
             data = link_el.text.split('\n')
 
+            movie_data_details = fetch_movie_detail(link)
+
             movie_lists_dict = {
                 'name': data[1],
                 'path': link,
@@ -23,7 +26,7 @@ def fetch_initial_data():
                 'nominations': data[4]
             }
             print(movie_lists_dict)
-
+            print(movie_data_details)
         except:
             pass
 
@@ -31,6 +34,8 @@ def fetch_initial_data():
             link = link_el.td.next.next.next['href']
             link = urljoin('https://en.wikipedia.org', link)
             data = link_el.text.split('\n')
+
+            movie_data_details = fetch_movie_detail(link)
 
             movie_lists_dict = {
                 'name': data[1],
@@ -40,8 +45,28 @@ def fetch_initial_data():
                 'nominations': data[4],
             }
             print(movie_lists_dict)
+            print(movie_data_details)
 
         except:
             pass
 
-fetch_initial_data()
+
+def fetch_movie_detail(link):
+    response = requests.get(link)
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    selector = '.infobox  tbody tr'
+    movie_detail_data_all = soup.select(selector)
+    details = {}
+    for movie_data in movie_detail_data_all:
+        title = movie_data.contents[0].text
+        try:
+            val = movie_data.contents[1].text.split('\n')
+        except:
+            val = ''
+        val = [i for i in val if i]
+        details[title] = val
+    return details
+
+
+get_movie_list_data()
